81a50abe58504d88ce99092d05ab2cd11ef5ac58
tyzjbclg <751865887@qq.com>
2026-05-08 16:03:09 +0800

    你的提交信息，比如: 修复so101相机配置

--- lerobot/common/robot_devices/robots/configs.py
+++ lerobot/common/robot_devices/robots/configs.py
@@ -443,7 +443,7 @@ class So101RobotConfig(ManipulatorRobotConfig):
     leader_arms: dict[str, MotorsBusConfig] = field(
         default_factory=lambda: {
             "main": FeetechMotorsBusConfig(
-                port="COM5",
+                port="COM19",
                 motors={
                     # name: (index, model)
                     "shoulder_pan": [1, "sts3215"],
@@ -460,7 +460,7 @@ class So101RobotConfig(ManipulatorRobotConfig):
     follower_arms: dict[str, MotorsBusConfig] = field(
         default_factory=lambda: {
             "main": FeetechMotorsBusConfig(
-                port="COM6",
+                port="COM20",
                 motors={
                     # name: (index, model)
                     "shoulder_pan": [1, "sts3215"],
@@ -478,12 +478,6 @@ class So101RobotConfig(ManipulatorRobotConfig):
         default_factory=lambda: {
             "laptop": OpenCVCameraConfig(
                 camera_index=0,
-                fps=30,
-                width=640,
-                height=480,
-            ),
-            "phone": OpenCVCameraConfig(
-                camera_index=2,
                 fps=30,
                 width=640,
                 height=480,
