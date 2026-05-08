b9056233ba075d2dc197e3ad0cce17fed0f42302
tyzjbclg <751865887@qq.com>
2026-05-08 16:05:34 +0800

    11

--- lerobot/common/robot_devices/robots/configs.py
+++ lerobot/common/robot_devices/robots/configs.py
@@ -482,6 +482,12 @@ class So101RobotConfig(ManipulatorRobotConfig):
                 width=640,
                 height=480,
             ),
+            # "phone": OpenCVCameraConfig(
+            #     camera_index=2,
+            #     fps=30,
+            #     width=640,
+            #     height=480,
+            # ),
         }
     )
 
